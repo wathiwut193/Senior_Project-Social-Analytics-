14f3b5456a74930c671f0fe6f0989ebaceb2d1ab
wathiwut193 <wathiwut193@gmail.com>
2019-01-24 13:30:15 +0700

    readfile with keyword

--- word_cut/listword_cut.py
+++ word_cut/listword_cut.py
@@ -6,27 +6,29 @@ Create on 22 /1 /2019 11:57
 import deepcut
 import time
 
-def newsreader():
+def newsreader_wordcut():
     start_time =time.time()
 
     """
     How :
-        Read text for file .txt and Cut word
+        Read text for file .txt and Cut word 
+        File manual input 
+        Output are write new file 
 
     Reference :
         Dictionary custom by add compond word from Wikipedia
     """
+    with open("news/input/2.txt",'r',encoding='utf-8') as readfile: #readfile
+        data = readfile.read()
+        word_cut = deepcut.tokenize(data, custom_dict='compond_word.txt')
 
-    read_news = open("news/input/2.txt",'r',encoding='utf-8')
-    word_cut = deepcut.tokenize(read_news.read(), custom_dict='compond_word.txt')
 
-    writenews = open("news/output/2_ตัด.txt",'w+',encoding='utf-8')
-    writenews.write('|'.join(word_cut))
+    with open("news/output/2_ตัด.txt",'w+',encoding='utf-8') as writefile: #writefile
+        writefile.read()
+        writefile.write('|'.join(word_cut))
+
     print(word_cut)
-    read_news.close()
-    writenews.close()
-
     finnish_time = time.time()
     print(finnish_time-start_time)
 
-newsreader()
+newsreader_wordcut()
