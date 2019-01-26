52505292fd30d98761bb3e0d90b43adfc11ee33c
JameDy77 <58160702@go.buu.ac.th>
2019-01-26 14:58:32 +0700

    add split "

--- word_cut/listword_cut.py
+++ word_cut/listword_cut.py
@@ -4,6 +4,7 @@ Create on 22 /1 /2019 11:57
 @Coder Wathiwut Kongjan
 """
 import deepcut
+import codecs
 import time
 
 def newsreader_wordcut():
@@ -18,17 +19,27 @@ def newsreader_wordcut():
     Reference :
         Dictionary custom by add compond word from Wikipedia
     """
-    with open("news/input/2.txt",'r',encoding='utf-8') as readfile: #readfile
+    with codecs.open("news/input/2.txt",'r',encoding='utf-8') as readfile: #readfile
         data = readfile.read()
-        word_cut = deepcut.tokenize(data, custom_dict='compond_word.txt')
+        cut = data.split('"')
+        readfile.close()
 
+    with codecs.open("news/output/2_ตัด.txt",'w+',encoding='utf-8') as write_split: #write_split
+        write_split.write(''.join(cut))
+        write_split.close()
+    
+    with codecs.open("news/output/2_ตัด.txt",'r+',encoding='utf-8') as readfile_split: #read_data
+        read_data = readfile_split.read()
+        word_cut = deepcut.tokenize(read_data, custom_dict='compond_word.txt')
+        readfile_split.close()
 
-    with open("news/output/2_ตัด.txt",'w+',encoding='utf-8') as writefile: #writefile
+    with codecs.open("news/output/2_ตัด.txt",'w+',encoding='utf-8') as writefile: #writefile
         writefile.read()
         writefile.write('|'.join(word_cut))
+        writefile.close()
 
     print(word_cut)
     finnish_time = time.time()
     print(finnish_time-start_time)
 
-newsreader_wordcut() #test
+newsreader_wordcut()
