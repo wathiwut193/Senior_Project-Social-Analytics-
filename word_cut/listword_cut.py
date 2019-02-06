3507fc978844a66118a64da6d9d5d15c6253a501
wathiwut193 <wathiwut193@gmail.com>
2019-02-06 17:20:55 +0700

    เพิ่ม corpus ข่าว

--- word_cut/listword_cut.py
+++ word_cut/listword_cut.py
@@ -6,8 +6,9 @@ Create on 22 /1 /2019 11:57
 import deepcut
 import time
 
+
 def newsreader_wordcut():
-    start_time =time.time()
+    start_time = time.time()
 
     """
     How :
@@ -16,19 +17,19 @@ def newsreader_wordcut():
         Output are write new file 
 
     Reference :
-        Dictionary custom by add compond word from Wikipedia
+        Dictionary custom by add compound word from Wikipedia
     """
-    with open("news/input/2.txt",'r',encoding='utf-8') as readfile: #readfile
+    with open("news/input/1.txt", 'r', encoding='utf-8') as readfile:  # readfile
         data = readfile.read()
-        word_cut = deepcut.tokenize(data, custom_dict='compond_word.txt')
+        word_cut = deepcut.tokenize(data, custom_dict='custom_dict_V1.txt')
 
-
-    with open("news/output/2_ตัด.txt",'w+',encoding='utf-8') as writefile: #writefile
+    with open("news/output/1_ตัด.txt", 'w+', encoding='utf-8') as writefile:  # writefile
         writefile.read()
         writefile.write('|'.join(word_cut))
 
     print(word_cut)
     finnish_time = time.time()
-    print(finnish_time-start_time)
+    print(finnish_time - start_time)
+
 
 newsreader_wordcut()
